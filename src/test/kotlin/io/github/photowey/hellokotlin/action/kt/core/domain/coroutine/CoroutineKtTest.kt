c50a8e2053f6ad6bcd22bf38ce4125de04326b62
photowey <photowey@gmail.com>
2026-01-10 22:34:23 +0800

    test(grammar): add unit tests for Kotlin scope functions (let, run, also) and coroutine builders
    Signed-off-by: photowey <photowey@gmail.com>

---
+++ src/test/kotlin/io/github/photowey/hellokotlin/action/kt/core/domain/coroutine/CoroutineKtTest.kt
@@ -0,0 +1,205 @@
+/*
+ * Copyright (c) 2026-present The Hello-Kotlin Authors. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.photowey.hellokotlin.action.kt.core.domain.coroutine
+
+import kotlinx.coroutines.*
+import java.util.concurrent.Executors
+import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.Duration.Companion.seconds
+
+/**
+ * {@code CoroutineKtTest}.
+ *
+ * @author photowey
+ * @version 1.0.0
+ * @since 2026/01/10
+ */
+object CoroutineKtTest {
+    // do nothing now.
+
+    suspend fun showUserInfo() {
+        println("Loading user...")
+        greet()
+        println("User: John Smith")
+    }
+
+    suspend fun greet() {
+        println("Hello world from a suspending function")
+        delay(1_000L)
+        println("The greet() on the thread: ${Thread.currentThread().name}")
+    }
+
+    suspend fun m1() {
+        showUserInfo()
+    }
+
+    suspend fun m2() {
+        withContext(Dispatchers.Default) {
+            this.launch { greet() }
+            println("The withContext() on the thread: ${Thread.currentThread().name}")
+        }
+    }
+
+    suspend fun m3() {
+        withContext(Dispatchers.Default) {
+            this.launch() {
+                greet()
+            }
+
+            // Starts another coroutine
+            this.launch() {
+                println("The CoroutineScope.launch() on the thread: ${Thread.currentThread().name}")
+                delay(1.seconds)
+                // The delay function simulates a suspending API call here
+                // You can add suspending API calls here like a network request
+            }
+
+            println("The withContext() on the thread: ${Thread.currentThread().name}")
+        }
+    }
+
+    suspend fun m4() {
+        // Root of the coroutine subtree
+        coroutineScope { // this: CoroutineScope
+            this.launch {
+                println("The coroutineScope-1 on the thread: ${Thread.currentThread().name}-${System.currentTimeMillis()}")
+                this.launch {
+                    println("The coroutineScope-2 on the thread: ${Thread.currentThread().name}-${System.currentTimeMillis()}")
+                    delay(2.seconds)
+                    println("Child of the enclosing coroutine completed-${System.currentTimeMillis()}")
+                }
+                println("Child coroutine 1 completed-${System.currentTimeMillis()}")
+            }
+            this.launch {
+                println("The coroutineScope-3 on the thread: ${Thread.currentThread().name}-${System.currentTimeMillis()}")
+                delay(1.seconds)
+                println("Child coroutine 2 completed-${System.currentTimeMillis()}")
+            }
+        }
+        // Runs only after all children in the coroutineScope have completed
+        println("Coroutine scope completed-${System.currentTimeMillis()}")
+    }
+
+    suspend fun m5() {
+        val singleThreadDispatcher = Executors.newSingleThreadExecutor().asCoroutineDispatcher()
+        singleThreadDispatcher.use { threadDispatcher ->
+            coroutineScope {
+                withContext(threadDispatcher) {
+                    this.launch {
+                        println("The coroutineScope-1 on the thread: ${Thread.currentThread().name}-${System.currentTimeMillis()}")
+                        this.launch {
+                            println("The coroutineScope-2 on the thread: ${Thread.currentThread().name}-${System.currentTimeMillis()}")
+                            delay(2.seconds)
+                            println("Child of the enclosing coroutine completed-${System.currentTimeMillis()}")
+                        }
+                        println("Child coroutine 1 completed-${System.currentTimeMillis()}")
+                    }
+                    this.launch {
+                        println("The coroutineScope-3 on the thread: ${Thread.currentThread().name}-${System.currentTimeMillis()}")
+                        delay(1.seconds)
+                        println("Child coroutine 2 completed-${System.currentTimeMillis()}")
+                    }
+                }
+            }
+            println("Coroutine scope completed-${System.currentTimeMillis()}")
+        }
+    }
+
+    suspend fun m6() {
+        coroutineScope {
+            withContext(Dispatchers.IO) {
+
+                val dispatcher = currentCoroutineContext()[CoroutineDispatcher]
+                println("Current dispatcher: $dispatcher")
+
+                launch {
+
+                    println("Dispatcher-1: ${currentCoroutineContext()[CoroutineDispatcher]}-${System.currentTimeMillis()}")
+
+                    println("The coroutineScope-1 on the thread: ${Thread.currentThread().name}-${System.currentTimeMillis()}")
+                    launch {
+
+                        println("Dispatcher-2: ${currentCoroutineContext()[CoroutineDispatcher]}-${System.currentTimeMillis()}")
+
+                        println("The coroutineScope-2 on the thread: ${Thread.currentThread().name}-${System.currentTimeMillis()}")
+                        delay(2.seconds)
+                        println("Child of the enclosing coroutine completed-${System.currentTimeMillis()}")
+                    }
+                    println("Child coroutine 1 completed-${System.currentTimeMillis()}")
+                }
+                launch {
+
+                    println("Dispatcher-3: ${currentCoroutineContext()[CoroutineDispatcher]}-${System.currentTimeMillis()}")
+
+                    println("The coroutineScope-3 on the thread: ${Thread.currentThread().name}-${System.currentTimeMillis()}")
+                    delay(1.seconds)
+                    println("Child coroutine 2 completed-${System.currentTimeMillis()}")
+                }
+            }
+        }
+        println("Coroutine scope completed-${System.currentTimeMillis()}")
+    }
+}
+
+suspend fun main_001() {
+    // CoroutineKtTest.m1()
+    // CoroutineKtTest.m2()
+    // CoroutineKtTest.m3()
+    // CoroutineKtTest.m4()
+    // CoroutineKtTest.m5()
+    CoroutineKtTest.m6()
+}
+
+suspend fun main_002() = withContext(Dispatchers.Default) { // this: CoroutineScope
+    // Starts downloading the first page
+    val firstPage = this.async {
+
+        delay(50.milliseconds)
+        "First page"
+    }
+
+    // Starts downloading the second page in parallel
+    val secondPage = this.async {
+        delay(100.milliseconds)
+        "Second page"
+    }
+
+    // Awaits both results and compares them
+    val pagesAreEqual = firstPage.await() == secondPage.await()
+    println("Pages are equal: $pagesAreEqual")
+}
+
+suspend fun main() = withContext(Dispatchers.Default) { // this: CoroutineScope
+    println("Running withContext block on ${Thread.currentThread().name}")
+
+    val one = this.async {
+        println("First calculation starting on ${Thread.currentThread().name}")
+        val sum = (1L..500_000L).sum()
+        delay(200L)
+        println("First calculation done on ${Thread.currentThread().name}")
+        sum
+    }
+
+    val two = this.async {
+        println("Second calculation starting on ${Thread.currentThread().name}")
+        val sum = (500_001L..1_000_000L).sum()
+        println("Second calculation done on ${Thread.currentThread().name}")
+        sum
+    }
+
+    // Waits for both calculations and prints the result
+    println("Combined total: ${one.await() + two.await()}")
+}
---
+++ src/test/kotlin/io/github/photowey/hellokotlin/action/kt/core/domain/scope/DataScopeUserTest.kt
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2026-present The Hello-Kotlin Authors. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.photowey.hellokotlin.action.kt.core.domain.scope
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+/**
+ * {@code DataScopeUserTest}.
+ *
+ * @author photowey
+ * @version 1.0.0
+ * @since 2026/01/10
+ */
+class DataScopeUserTest {
+
+    @Test
+    fun testScopeFunctions() {
+        val user = DataScopeUser(
+            10086L,
+            "photowey",
+            18
+        )
+
+        // ----------------------------------------------------------------
+
+        val letRvt = user.let { innerUser: DataScopeUser ->
+            "let::${innerUser.javaClass}"
+        }
+
+        val letVal = "let::class io.github.photowey.hellokotlin.action.kt.core.domain.scope.DataScopeUser"
+        assertEquals(letVal, letRvt)
+
+        val runRvt = user.run {
+            "run::${this.javaClass}"
+        }
+        val runVal = "run::class io.github.photowey.hellokotlin.action.kt.core.domain.scope.DataScopeUser"
+        assertEquals(runVal, runRvt)
+
+        // ----------------------------------------------------------------
+
+        user.also {
+            println("also::${it.javaClass}")
+        }
+        user.apply {
+            println("apply::${this.javaClass}")
+        }
+
+        // ----------------------------------------------------------------
+
+        user.takeIf { it.age > 16 }?.also { println("The name is:${it.name}!") } ?: println("The name is empty!")
+        user.takeUnless { it.age > 16 }?.also { println("The name is empty!") } ?: println("The name is:${user.name}!")
+
+        repeat(2) {
+            print(it)
+            println(user.name)
+        }
+
+        with(user) {
+            this.name = "Tom"
+        }
+
+        assertEquals("Tom", user.name)
+    }
+}
